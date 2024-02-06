0820393b42a2cc79e1df65b3c7ba14274e10fc7d
chengfengshaner <157273073+chengfengshaner@users.noreply.github.com>
2024-02-06 23:50:24 +0800

    Add files via upload

---
+++ 常见关键字/test.c
@@ -0,0 +1,21 @@
+//常见关键字 auto break case char const continue default do double else enum
+//extern float for goto if int long register return short signed
+//sizeof static struct switch typedef union unsigned void volatile while
+//注：目前只介绍如下几条
+
+#include <stdio.h>
+
+//C语言提供的关键字
+//1.C语言提供的，不能自己创造的
+//2.变量名不能是关键字
+ 
+int main()
+{
+	int char； - 此处即错误的方式
+	return 0;
+}
+
+
+
+
+
